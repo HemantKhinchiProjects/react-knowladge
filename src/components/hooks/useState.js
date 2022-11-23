ac81603bfbd11c9a39054f01139d444aaa77df85
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2022-11-23 16:47:18 +0530

    2 files added, 1 deleted

--- src/components/hooks/useState.js
+++ src/components/hooks/useState.js
@@ -9,10 +9,11 @@
 3.And that's the use effect hook there make sure that you always add everything you refer to inside of use effect as a dependency unless there is a good reason not to do that.*/
 
 //UseState
-/*is hooks ka use hum value ko update karne ke liye karte hai. jub hum ise call karte hai to humare pass 2 argument hote hai,
+/*is hooks ka use hum value ko update karne ke liye karte hai. ye usi component ko update karega jusko hum update karana chahate hai. dusare component pe iska effect nahi hoga. hum multipal Usestate use kar sakte hai. jub hum ise call karte hai to humare pass 2 argument hote hai,
 1. varble hota hai junha hume batana hota hai ki hume kon se value update karni hai.
 2. dusara function hota hai jiski help se hum number 1 var ki value update/change kar sakte hai.
 */
+// Note: aksar hum state ko value update karne ke liye onclick ka use karte hai. but esa jaruri nahi hai ki hum ise click event pe hi update karye. bina click ke bhi hum ye kar sakte hai.humare pass koi bhi karan ho hum update kar sakte hai. for example HTTP Request update.
 //Example:
 import React, { useState } from 'react';
 
@@ -33,3 +34,4 @@ function ExpenseItem(props) {
     </div>
   );
 }
+//state can be update in many way
