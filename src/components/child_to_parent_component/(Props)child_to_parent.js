a6859d88ea66a57b7514942deb193186d50bbe9b
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2022-11-24 15:39:00 +0530

    2 files modified, 2 added

---
+++ src/components/child_to_parent_component/(Props)child_to_parent.js
@@ -0,0 +1,51 @@
+
+//This is supper importent pattren to communicate between components. jis tarha se react main hum data props ki help se parent to child bhejate hai usi ko use karke hum apna deta child to parent bhi bhej sakte hai. par iske liye hum bhich mai jitane bhi component honge unko skeep nahi kar sakte. unko sabhi ko use karke hi hum deta lift kar sakte hai. props mai hu kewal component to component hi deta bhej sakte hai. kisi bhi component ko skeep nahi kar sakte.
+//Example:
+/*----------componet B se data ko component a mai bhej rahe hai.
+/*----------componet A se data ko component a mai Access kar rahe hai rahe hai.
+//-----------------------------Component A----------------------------------*/
+
+import React from 'react';
+import './NewExpense.css';
+import ExpenseForm from './ExpenseForm';
+const NewExpense = () => {
+  const SaveExpenseDataHandler = (entredExpenseData) => {
+    const expenseData = {
+      ...entredExpenseData,
+      id: Math.random().toString(),
+    };
+    console.log(expenseData);
+  };
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={SaveExpenseDataHandler} />
+    </div>
+  );
+};
+export default NewExpense;
+
+//------------------------Component B-----------------------------------*/
+
+import React, { useState } from 'react';
+const ExpenseForm = (props) => {
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: enterdTitle,
+      amount: enterdAmount,
+      date: new Date(enterdDate),
+    };
+    //for all input blank
+    SetenterdTitle('');
+    SetenterdAmount('');
+    SetenterdDate('');
+
+    props.onSaveExpenseData(expenseData); //Note: ynha se hum object ko onSaveExpenseData function ke sath parent componet ko bhej rahe hai.
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      </form>
+  );
+};
+export default ExpenseForm;
+
--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,3 +1,16 @@
+/*Two way binding?*/
+// useState mai hum na kewal input lete hai bulki use update karke wapas bhi karte hai ye hi two-way binding kahalata hai. jesa ki form input 'value' mai humne kiya hai
+Example:
+
+const [enterdTitle, SetenterdTitle] = useState('');
+
+const amountChangeHandler = (event) => {
+  SetenterdTitle(event.target.value);
+};
+
+<input type="number"onChange={amountChangeHandler} value={enterdTitle}/>
+
+
 /*Listing to user input
 //Focus Events
 onFocus
