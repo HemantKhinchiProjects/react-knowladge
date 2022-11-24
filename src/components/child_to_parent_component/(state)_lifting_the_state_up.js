6e98d41cc20004c03f998287d03781d2f21bc555
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2022-11-24 15:51:16 +0530

    1 file modified

---
+++ src/components/child_to_parent_component/(state)_lifting_the_state_up.js
@@ -0,0 +1,6 @@
+//Scenario
+/*1: App
+2:Expenses
+3:New_Expenses
+*/
+// Jub humne data "New_Expenses" component mai generat kiya hai. lekin us deta ki jarurat "Expenses" component  ko hai. to is condistion mai hum data "App" component ko use karte huye "Expenses" component ko pass karenge.  is pattren ko [state lifting] bolte hai. data hum direct nahi bhej sakte.
