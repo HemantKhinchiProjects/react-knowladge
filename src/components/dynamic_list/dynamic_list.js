cb2e8f4df9e319ba7e85c2f32d493144aa20f07b
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2022-11-24 18:49:02 +0530

    1 file modified

--- src/components/dynamic_list/dynamic_list.js
+++ src/components/dynamic_list/dynamic_list.js
@@ -1,6 +1,6 @@
 // mostly use build-in Array methos map()
 // map()
-// map ek naya Array create karta hi. jo ki dusare Array pe based hota hai. jo ki purane Array ke sabhi Element ko cheack karke new Array create karta hai.
+// map ek naya Array create karta hi. jo ki dusare Array pe based hota hai. jo ki purane Array ke sabhi Element ko cheack karke new Array create kar ke deta hai.
 
 //Example:-
 import React, { useState } from 'react';
@@ -22,9 +22,52 @@ function Expenses(props) {
             amount={expense.amount}
             date={expense.date}
           />
-        ))}
+        ))} // new array create over there from billow file
       </Card>
     </>
   );
 }
 export default Expenses;
+
+
+// file number 2
+
+import React from 'react';
+import './style.css';
+
+import Expenses from './components/expenseItem/Expenses';
+import NewExpense from './components/newExpense/NewExpense';
+export default function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+  const addExpenseHandler = (expenses) => {
+    console.log('In App.js');
+    console.log(expenses);
+  };
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  );
+}
+
