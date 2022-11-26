40e70007415ec66d7d7fe73d91232b8ef59e0f02
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2022-11-26 18:28:49 +0530

    1 file modified

--- src/components/Very_important_child_to_parent_component/(state)_lifting_the_state_up.js
+++ src/components/Very_important_child_to_parent_component/(state)_lifting_the_state_up.js
@@ -3,4 +3,4 @@
 2:Expenses
 3:New_Expenses
 */
-// Jub humne data "New_Expenses" component mai generat kiya hai. lekin us deta ki jarurat "Expenses" component  ko hai. to is condistion mai hum data "App" component ko use karte huye "Expenses" component ko pass karenge. kyo ki humare pass 2 sublings ke bich koi direct connection nahi hota hai.data hum direct nahi bhej sakte.is condistion main hum data [closest parent component]ki help se bhej sakte hai, is pattren ko [state lifting] bolte hai.
+// Jub humne data "New_Expenses" component mai generat kiya hai. lekin us deta ki jarurat "Expenses" component  ko hai. to is condistion mai hum data "App" component ko use karte huye "Expenses" component ko pass karenge. kyo ki humare pass 2 sublings ke bich koi direct connection nahi hota hai.data hum direct nahi bhej sakte.is condistion main hum data [closest parent component]ki help se bhej sakte hai, is pattren ko [state lifting] bolte hai. 
