da5fbf5abda2693da295ab7f3b7ff602f6cc3f5c
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2022-11-28 14:05:13 +0530

    1 file modified

--- src/components/Condistion/condistion.js
+++ src/components/Condistion/condistion.js
@@ -5,10 +5,25 @@ import React, {  } from 'react';
 
 const Expenses = (props) => {
   // console.log(props.items);
+  /*-----------Simple javascript condistion related vanila javascript pattren point------*/
+  let expensesContent = <p>No expenses Found </p>;
+  if(filteredExpenses.length > 0){
+    expensesContent =  filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
+  }
+
   
   return (
     <div>
-        {} //Condistion code will come here between curly-Brackets 
+        {} //Condistion code will come here between curly-Brackets
+        /*---------Using vanila javascript pattren*/ 
+        {expensesContent}
         /*-------------------//simple condistion--------------*/
         {filteredExpenses.length === 0 && <p>No expenses Found </p>}
         { filteredExpenses.length >= 0 && filteredExpenses.map((expense) => (
