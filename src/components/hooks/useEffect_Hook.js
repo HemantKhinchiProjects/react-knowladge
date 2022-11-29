87d1f0ab1cdf208259ec0997164b014130859f81
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2022-11-29 14:44:45 +0530

    1 file modified

--- src/components/hooks/useEffect_Hook.js
+++ src/components/hooks/useEffect_Hook.js
@@ -1,4 +1,6 @@
-agar hume react se bahar ke world se judna chahate hai to jese ki agar hum API use kar rahe hai. then we use the useEffect hook. useEffect hooks ek side effect ki tarha kam karta hai. matlab esa oprastion karna jo humari application ke bahar mojud ho. jiska anuman lagane yogya parinam nahi hai. iska basic syntex main pahala argument ek  function hota hai. or dusara argument  ek Array hota hai.
+agar hume react ko bahar ke world se judna chahate hai, jese ki agar hum API use kar rahe hai. then we use the useEffect hook. useEffect hooks ek side effect ki tarha kam karta hai. matlab esa oprastion karna jo humari application ke bahar mojud ho. jiske result ka hum anuman nahi laga sakte hai. 
+
+iska basic syntex main pahala argument ek  function hota hai. or dusara argument  ek Array hota hai.
 
 //Eexample:
 import { useEffect } from 'react';
@@ -9,6 +11,8 @@ function MyComponent() {
    }, []);
 }
 Note: agar hum koi data fatch karna chahate hai to hume useEffect ka use karna chiye. jese koi list lane or dikhane main. agr hume effect se milne wali value ko use karne ki jarurat hai to ise dependencies array mai rakhne ki jarurat hai.
+
+
 
 //Eexample:
 import { useEffect } from 'react';
@@ -23,4 +27,19 @@ function PostList() {
    }, []);
 
    return posts.map(post => <Post key={post.id} post={post} />
+}
+note: agar value change hoti hai to effect function fir se re-executed hota hai.
+//For example, here is a bit of code that adds or removes the class “overflow-hidden” to the body element whenever the mobile menu is opened or closed.
+function Mobile({ open }) {
+  useEffect(() => {
+    const body = document.querySelector("#__next");
+
+    if (open) {
+      body.classList.add("overflow-hidden");
+    } else {
+      body.classList.remove("overflow-hidden");
+    }
+  }, [open]);
+ 
+  // ...
 }
