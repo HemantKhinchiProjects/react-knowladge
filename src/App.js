e8120c5d2ec76dd2ec1cb1cd4ec66df2ce9f7a65
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2023-01-11 16:44:26 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ export default function App() {
       <h1>Hello Hemant!</h1>
       <p>Start Example of Hooks :)</p>
       <div className="alert alert-success" role="alert">
-        <h4 className="alert-heading">This can improve Performance.!</h4>
+        <h4 className="alert-heading">
+          useMemo This can improve Performance.!
+        </h4>
         <p>
           The useMemo and useCallback Hooks are similar. The main difference is
           that useMemo returns a memoized value and useCallback returns a
@@ -22,6 +24,21 @@ export default function App() {
         </p>
       </div>
       <UMemo />
+      <div className="alert alert-success" role="alert">
+        <h4 className="alert-heading">
+          useCallback This can improve Performance.!
+        </h4>
+        <p>
+          The useMemo and useCallback Hooks are similar. The main difference is
+          that useMemo returns a memoized value and useCallback returns a
+          memoized function.
+        </p>
+        <hr />
+        <p className="mb-0">
+          Whenever you need to, be sure to use margin utilities to keep things
+          nice and tidy.
+        </p>
+      </div>
       <Callback />
     </div>
   );
--- src/Hooks/useCallback/Callback.js
+++ src/Hooks/useCallback/Callback.js
@@ -2,22 +2,12 @@ import React, { useState, useMemo } from 'react';
 import Child from './child/Child';
 const UMemo = () => {
   const [value, setValue] = useState(0);
-  const [valueTwo, setValueTwo] = useState(0);
 
   const handlerUpdate = () => {
     console.log('01');
     setValue(value + 1);
   };
-  const handlerUpdateTwo = () => {
-    console.log('02');
-    setValueTwo(valueTwo + 1);
-  };
-  // const isEven = () => {
-  //   console.log('-------------------------');
-  //   let i = 0;
-  //   while (i < 200000000) i++;
-  //   return value % 2 === 0;
-  // }; //This function call unnessary in handlerUpdateTwo so application purformance will slow. so will improve purfomance i will use usememo below
+
   const isEven = useMemo(() => {
     let i = 0;
     while (i < 200000000) i++;
@@ -25,7 +15,6 @@ const UMemo = () => {
   }, [value]);
   return (
     <div>
-      <h4>Use Callback</h4>
       <Child />
       <p>This is {isEven ? 'Even Number' : 'Odd Number'}</p>
       <button
@@ -34,13 +23,6 @@ const UMemo = () => {
         onClick={handlerUpdate}
       >
         increase {value}
-      </button>
-      <button
-        type="button"
-        className="btn btn-outline-danger"
-        onClick={handlerUpdateTwo}
-      >
-        Dicrease {valueTwo}
       </button>
     </div>
   );
