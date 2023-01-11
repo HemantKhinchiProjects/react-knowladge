f63018d2b9af7f10b0e98b94aa8e8d4bf4d32f5f
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2023-01-11 12:58:13 +0530

    1 file modified

--- src/Hooks/useMemo/Memo.js
+++ src/Hooks/useMemo/Memo.js
@@ -1,5 +1,15 @@
-import React,{} from 'react';
+import React from 'react';
 const UMemo = () => {
-  return <div>Use memo</div>;
+  return (
+    <div>
+      <h4>Use memo</h4>
+      <button type="button" class="btn btn-primary btn-lg">
+        Large button
+      </button>
+      <button type="button" class="btn btn-secondary btn-lg">
+        Large button
+      </button>
+    </div>
+  );
 };
 export default UMemo;
