8cf7dc268c7ccc4f7d7f9cd4b560451b65b049e0
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2023-01-11 14:13:51 +0530

    1 file modified

--- src/Hooks/useMemo/Memo.js
+++ src/Hooks/useMemo/Memo.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
 const UMemo = () => {
   const [value, setValue] = useState(0);
-
+  const handlerUpdate = () => {
+    setValue(value + 1);
+  };
   return (
     <div>
       <h4>Use memo</h4>
-      <button type="button" class="btn btn-outline-primary">
+      <button
+        type="button"
+        class="btn btn-outline-primary"
+        onClick={handlerUpdate}
+      >
         Primary {value}
       </button>
       <button type="button" class="btn btn-outline-secondary">
