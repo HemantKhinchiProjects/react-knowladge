948c6e892482bbf35a1adc82d097f218b81b0708
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2023-01-11 15:20:02 +0530

    1 file modified

--- src/Hooks/useMemo/Memo.js
+++ src/Hooks/useMemo/Memo.js
@@ -1,21 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 const UMemo = () => {
   const [value, setValue] = useState(0);
+  const [valueTwo, setValueTwo] = useState(0);
+
   const handlerUpdate = () => {
+    console.log('01');
     setValue(value + 1);
   };
+  const handlerUpdateTwo = () => {
+    console.log('02');
+    setValueTwo(valueTwo + 1);
+  };
+  // const isEven = () => {
+  //   console.log('-------------------------');
+  //   let i = 0;
+  //   while (i < 200000000) i++;
+  //   return value % 2 === 0;
+  // }; //This function call unnessary in handlerUpdateTwo so application purformance will slow. so will improve purfomance i will use usememo below
+  const isEven = useMemo(() => {
+    let i = 0;
+    while (i < 200000000) i++;
+    return value % 2 === 0;
+  }, [value]);
   return (
     <div>
       <h4>Use memo</h4>
+      <p>This is {isEven ? 'Even Number' : 'Odd Number'}</p>
       <button
         type="button"
-        class="btn btn-outline-primary"
+        className="btn btn-outline-primary"
         onClick={handlerUpdate}
       >
-        Primary {value}
+        increase {value}
       </button>
-      <button type="button" class="btn btn-outline-secondary">
-        Secondary
+      <button
+        type="button"
+        className="btn btn-outline-danger"
+        onClick={handlerUpdateTwo}
+      >
+        Dicrease {valueTwo}
       </button>
     </div>
   );
