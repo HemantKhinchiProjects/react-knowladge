252f7ff4c5ed97c63a3a6a72f4e394a0a8c61dad
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2023-01-12 16:59:28 +0530

    1 file modified

--- src/Hooks/useRef/Ref.js
+++ src/Hooks/useRef/Ref.js
@@ -1,10 +1,12 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
 const Ref = () => {
   //its like a useState only and its preserve the value. No rerander
   const luckyName = useRef(null);
+  const [show, setShow] = useState(false);
   const handlerSubmit = (e) => {
     e.preventDefault();
-    console.log(luckyName);
+    const yname = luckyName.current.value;
+    yname == '' ? alert('Please Fill the data') : setShow(true);
   };
   return (
     <div className="my-2 alert alert-dark ">
@@ -18,13 +20,11 @@ const Ref = () => {
             className="form-control"
             id="luckyName"
             ref={luckyName}
-            aria-describedby="emailHelp"
           />
         </div>
-        <button type="button" className="btn btn-primary">
-          Submit
-        </button>
+        <button className="btn btn-primary">Submit</button>
       </form>
+      <small>{show ? `Your name is ${luckyName.current.value}` : show} </small>
     </div>
   );
 };
